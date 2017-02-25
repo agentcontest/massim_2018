f9c77b9e030d700416cd5a31ec33892254d3fc2c
Sarah Diedrich <sarah.diedrich93@gmail.com>
2017-02-25 14:21:16 +0100

    add item parameters to TestConfig and parse item parameters from config

--- server/src/main/java/massim/scenario/city/util/Generator.java
+++ server/src/main/java/massim/scenario/city/util/Generator.java
@@ -2,6 +2,7 @@ package massim.scenario.city.util;
 
 import massim.scenario.city.data.*;
 import massim.scenario.city.data.facilities.*;
+import massim.util.Log;
 import org.json.JSONObject;
 
 import java.util.*;
@@ -12,8 +13,110 @@ import java.util.stream.Collectors;
  */
 public class Generator {
 
+    private int baseItemsMin;
+    private int baseItemsMax;
+    private int levelDecreaseMin;
+    private int levelDecreaseMax;
+    private int graphDepthMin;
+    private int graphDepthMax;
+    private int resourcesMin;
+    private int resourcesMax;
+
+    private int minVol;
+    private int maxVol;
+    private int valueMin;
+    private int valueMax;
+    private int minReq;
+    private int maxReq ;
+    private int reqAmountMin;
+    private int reqAmountMax;
+
+    private int toolsMin;
+    private int toolsMax;
+    private double toolProbability;
+
+
     public Generator(JSONObject randomConf){
         // TODO parse random parameters from config
+
+        //parse facilities
+        JSONObject facilities = randomConf.optJSONObject("facilities");
+        if(facilities == null){
+            Log.log(Log.Level.ERROR, "No facilities in configuration.");
+        }else{
+
+            //parse charging stations
+            JSONObject chargingStations = facilities.optJSONObject("chargingStations");
+            if(chargingStations == null){
+                Log.log(Log.Level.ERROR, "No charging stations in configuration.");
+            }else{
+                String density = chargingStations.optString("density", "0.8");
+                Log.log(Log.Level.NORMAL, "Configuring facilities charging station density: " + density);
+            }
+            //parse shops
+
+            //parse dumps
+
+            //parse workshops
+
+            //parse storage
+        }
+
+
+        //parse items
+        JSONObject items = randomConf.optJSONObject("items");
+        if(items == null){
+            Log.log(Log.Level.ERROR, "No items in configuration.");
+        }else{
+            baseItemsMin = items.optInt("baseItemsMin",5);
+            Log.log(Log.Level.NORMAL, "Configuring items baseItemsMin: " + baseItemsMin);
+            baseItemsMax = items.optInt("baseItemsMax",7);
+            Log.log(Log.Level.NORMAL, "Configuring items baseItemsMax: " + baseItemsMax);
+            levelDecreaseMin = items.optInt("levelDecreaseMin",1);
+            Log.log(Log.Level.NORMAL, "Configuring items levelDecreaseMin: " + levelDecreaseMin);
+            levelDecreaseMax = items.optInt("levelDecreaseMax",2);
+            Log.log(Log.Level.NORMAL, "Configuring items levelDecreaseMax: " + levelDecreaseMax);
+            graphDepthMin = items.optInt("graphDepthMin",3);
+            Log.log(Log.Level.NORMAL, "Configuring items graphDepthMin: " + graphDepthMin);
+            graphDepthMax = items.optInt("graphDepthMax",4);
+            Log.log(Log.Level.NORMAL, "Configuring items graphDepthMax: " + graphDepthMax);
+            resourcesMin = items.optInt("resourcesMin",1);
+            Log.log(Log.Level.NORMAL, "Configuring items resourcesMin: " + resourcesMin);
+            resourcesMax = items.optInt("resourcesMax",1);
+            Log.log(Log.Level.NORMAL, "Configuring items resourcesMax: " + resourcesMax);
+
+            minVol = items.optInt("minVol",10);
+            Log.log(Log.Level.NORMAL, "Configuring items minVol: " + minVol);
+            maxVol = items.optInt("maxVol",100);
+            Log.log(Log.Level.NORMAL, "Configuring items maxVol: " + maxVol);
+            valueMin = items.optInt("valueMin",10);
+            Log.log(Log.Level.NORMAL, "Configuring items valueMin: " + valueMin);
+            valueMax = items.optInt("valueMax",100);
+            Log.log(Log.Level.NORMAL, "Configuring items valueMax: " + valueMax);
+            minReq = items.optInt("minReq",1);
+            Log.log(Log.Level.NORMAL, "Configuring items minReq: " + minReq);
+            maxReq = items.optInt("maxReq",3);
+            Log.log(Log.Level.NORMAL, "Configuring items maxReq: " + maxReq);
+            reqAmountMin = items.optInt("reqAmountMin",1);
+            Log.log(Log.Level.NORMAL, "Configuring items reqAmountMin: " + reqAmountMin);
+            reqAmountMax = items.optInt("reqAmountMax",3);
+            Log.log(Log.Level.NORMAL, "Configuring items reqAmountMax: " + reqAmountMax);
+
+            toolsMin = items.optInt("toolsMin",1);
+            Log.log(Log.Level.NORMAL, "Configuring items toolsMin: " + toolsMin);
+            toolsMax = items.optInt("toolsMax",1);
+            Log.log(Log.Level.NORMAL, "Configuring items toolsMax: " + toolsMax);
+            toolProbability = items.optDouble("toolProbability",0.5);
+            Log.log(Log.Level.NORMAL, "Configuring items toolProbability: " + toolProbability);
+        }
+
+        //parse jobs
+        JSONObject jobs = randomConf.optJSONObject("jobs");
+        if(jobs == null){
+            Log.log(Log.Level.ERROR, "No jobs in configuration.");
+        }else{
+
+        }
     }
 
     /**
