fb19b04a094915e81d7b1b556db30c198c38ae03
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-03-29 18:59:43 +0200

    start creating web client

---
+++ monitor/gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const browserify = require('browserify');
+const tsify = require('tsify');
+const watchify = require('watchify');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
+
+function build() {
+  return browserify('js/main.ts')
+    .plugin(tsify);
+}
+
+const watchedBrowserify = watchify(build());
+
+function bundle() {
+  return watchedBrowserify
+    .bundle()
+    .on('error', (e) => gutil.log(gutil.colors.red(e.message)))
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('www/'));
+}
+
+gulp.task('default', [], bundle);
+watchedBrowserify.on('update', bundle);
+watchedBrowserify.on('log', gutil.log);
