0e71882848900398e3fda0f810e680525e0c2aba
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-03-29 20:36:45 +0200

    render a map

--- monitor/js/main.ts
+++ monitor/js/main.ts
@@ -1,3 +1,5 @@
+/// <reference path="../dts/openlayers.d.ts" />
+
 import { init } from 'snabbdom';
 import { VNode } from 'snabbdom/vnode';
 
@@ -9,11 +11,14 @@ import listeners from 'snabbdom/modules/eventlisteners';
 import view from './view';
 import makeCtrl from './ctrl';
 import { Ctrl } from './interfaces';
+import ol = require('openlayers');
 
 const patch = init([klass, props, attributes, listeners]);
 
-export default function Monitor(element: Element) {
-  let vnode: VNode | Element = element;
+const CLAUSTHAL: ol.Coordinate = [10.340707, 51.8080063];
+
+export default function Monitor(mapElement: Element, overlayElement: Element) {
+  let vnode: VNode | Element = mapElement;
   let ctrl: Ctrl;
 
   function redraw() {
@@ -21,6 +26,27 @@ export default function Monitor(element: Element) {
   }
 
   ctrl = makeCtrl(redraw);
+  //patch(element, view(ctrl));
 
-  patch(element, view(ctrl));
+  const vectorSource = new ol.source.Vector();
+
+  const openStreetMapLayer = new ol.layer.Tile({
+    source: new ol.source.OSM({
+      attributions: [ol.source.OSM.ATTRIBUTION],
+      url: 'http://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    })
+  });
+
+  const vectorLayer = new ol.layer.Vector({
+    source: vectorSource
+  });
+
+  /* const map = */ new ol.Map({
+    target: mapElement,
+    layers: [openStreetMapLayer, vectorLayer],
+    view: new ol.View({
+      center: ol.proj.fromLonLat(CLAUSTHAL),
+      zoom: 15
+    })
+  });
 }
