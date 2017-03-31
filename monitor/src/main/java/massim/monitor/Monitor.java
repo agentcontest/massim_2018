5fa5d7f500a8a8edad9b2c8ec2f353fc17f4e62b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-03-31 15:06:40 +0200

    send latest available data to new clients

--- monitor/src/main/java/massim/monitor/Monitor.java
+++ monitor/src/main/java/massim/monitor/Monitor.java
@@ -6,12 +6,15 @@ import massim.protocol.scenario.city.data.StaticCityData;
 
 import org.webbitserver.WebServers;
 import org.webbitserver.WebServer;
+import org.webbitserver.BaseWebSocketHandler;
+import org.webbitserver.WebSocketConnection;
 import org.webbitserver.handler.StaticFileHandler;
 import org.webbitserver.handler.HttpToWebSocketHandler;
 
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+import java.util.HashSet;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -19,7 +22,25 @@ import java.util.concurrent.ExecutionException;
  */
 public class Monitor {
 
-    private final SocketHandler socketHandler = new SocketHandler();
+    private String latestStatic;
+    private String latestDynamic;
+
+    private final HashSet<WebSocketConnection> pool = new HashSet<WebSocketConnection>();
+
+    private final BaseWebSocketHandler socketHandler = new BaseWebSocketHandler() {
+        @Override
+        public void onOpen(WebSocketConnection client) {
+            pool.add(client);
+            if (latestStatic != null) client.send(latestStatic);
+            if (latestDynamic != null) client.send(latestDynamic);
+        }
+
+        @Override
+        public void onClose(WebSocketConnection client) {
+            pool.remove(client);
+        }
+
+    };
 
     /**
      * Constructor.
@@ -41,22 +62,27 @@ public class Monitor {
         // TODO
     }
 
+    private void broadcast(String message) {
+        for (WebSocketConnection client: this.pool) {
+            client.send(message);
+        }
+    }
+
     /**
      * Updates the current state of the monitor.
      * Called by the massim server after each step.
      */
     public void updateState(WorldData worldData){
         if (worldData instanceof StaticCityData) {
-            updateStaticCityData((StaticCityData) worldData);
+            this.latestStatic = staticToJson((StaticCityData) worldData);
+            this.broadcast(this.latestStatic);
         } else if (worldData instanceof DynamicCityData){
-            updateDynamicCityData((DynamicCityData) worldData);
-        } else {
-            System.out.println("Monitor: wrong scenario");
-            return;
+            this.latestDynamic = dynamicToJson((DynamicCityData) worldData);
+            this.broadcast(this.latestDynamic);
         }
     }
 
-    private void updateStaticCityData(StaticCityData data) {
+    private String staticToJson(StaticCityData data) {
         JSONObject d = new JSONObject();
         d.put("simId", data.simId);
         d.put("steps", data.steps);
@@ -65,10 +91,10 @@ public class Monitor {
         d.put("teams", data.teams);
         d.put("roles", data.roles);
         d.put("items", data.items);
-        this.socketHandler.broadcast(d.toString());
+        return d.toString();
     }
 
-    private void updateDynamicCityData(DynamicCityData data) {
+    private String dynamicToJson(DynamicCityData data) {
         JSONObject d = new JSONObject();
         d.put("workshops", data.workshops);
         d.put("chargingStations", data.chargingStations);
@@ -77,6 +103,6 @@ public class Monitor {
         d.put("resourceNodes", data.resourceNodes);
         d.put("storages", data.storages);
         d.put("entities", data.entities);
-        this.socketHandler.broadcast(d.toString());
+        return d.toString();
     }
 }
