1b6724b6f6d42acb2bbfec01ac9056e274a36afc
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2017-07-10 19:20:50 +0200

    add reference for SimEnd class for jaxb

--- protocol/src/main/java/massim/protocol/MessageContent.java
+++ protocol/src/main/java/massim/protocol/MessageContent.java
@@ -1,16 +1,13 @@
 package massim.protocol;
 
-import massim.protocol.messagecontent.Action;
-import massim.protocol.messagecontent.AuthRequest;
-import massim.protocol.messagecontent.AuthResponse;
-import massim.protocol.messagecontent.Bye;
+import massim.protocol.messagecontent.*;
 
 import javax.xml.bind.annotation.XmlSeeAlso;
 
 /**
  * The content of a message following the MASSim protocol.
  */
-@XmlSeeAlso({AuthRequest.class, AuthResponse.class, Bye.class, Action.class})
+@XmlSeeAlso({AuthRequest.class, AuthResponse.class, Bye.class, Action.class, SimEnd.class})
 public abstract class MessageContent {
 
     /**
